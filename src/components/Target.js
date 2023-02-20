e353ef7c9f126699cf166788fc918324e8549ea7
Jobwafula <jobwafulabg@gmail.com>
2023-02-20 23:53:23 +0300

    jumbotron

--- src/components/Target.js
+++ src/components/Target.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Target() {
   return (
-    <div className='row' id='row-target'>
-                <div className='col-sm-6'>
+    <div className='row container-fluid jumbotron' id='row-target'>
+                <div className='col-sm-6 '>
                     <h1>With the recent recent  <span style={{color:'red'}}>increase in scams</span> targeting job seekers</h1>
                     <h3>we have put together helpful tips and resources to keep you safe</h3>
                 </div>
