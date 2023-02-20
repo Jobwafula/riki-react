e9efcea51bd5ec605fafdcebddd24a2eda8218db
Jobwafula <jobwafulabg@gmail.com>
2023-02-20 23:55:59 +0300

    initial commit

---
+++ src/components/data.js
@@ -0,0 +1,25 @@
+ data = [
+    {
+        id:1,
+        img:"",
+        title:"Fair Pay",
+        ethicmsg:"Our goal is to pay our crowd above minimum wage in every market around the world where we operate."
+    },
+    {
+        id:2,
+        img:"",
+        title:"inclusion",
+        ethicmsg:"A diverse, inclusive culture is vital to our mission of helping build better AI. We offer opportunities for individuals of all abilities and backgrounds."
+        
+    },
+    {
+        id:3,
+        img:"",
+        title:"Crowd Voice",
+        ethicmsg:"Our crowd has a valued voice at Appen and their feedback helps us to continuously improve."
+    }
+]
+
+export default data;
+
+console.log(data)
