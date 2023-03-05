8cfafd8e69d3e83198e70ea9619a03f8671b7c2d
Jobwafula <jobwafulabg@gmail.com>
2023-03-05 13:59:04 +0300

    mapping

--- src/components/data.js
+++ src/components/data.js
@@ -1,25 +1,27 @@
- data = [
-    {
-        id:1,
+ const data =  [
+    {   id:1,
         img:"",
-        title:"Fair Pay",
-        ethicmsg:"Our goal is to pay our crowd above minimum wage in every market around the world where we operate."
+        title:"fair,flexible work",
+        text:"Manage your work-life balance with flexible, part-time work from the comfort of your home, while working on interesting projects with competitive pay."
     },
     {
         id:2,
         img:"",
-        title:"inclusion",
-        ethicmsg:"A diverse, inclusive culture is vital to our mission of helping build better AI. We offer opportunities for individuals of all abilities and backgrounds."
-        
+        title:"work for everyone",
+        text:"With numerous projects available at all times, you can choose to work on a diverse set of tasks, like reviewing ads, search results, and map points, labeling images and video for self-driving cars, transcribing audio clips for voice assistants, translating text snippets, and more."
     },
     {
         id:3,
         img:"",
-        title:"Crowd Voice",
-        ethicmsg:"Our crowd has a valued voice at Appen and their feedback helps us to continuously improve."
+        title:"shape the world",
+        text:"Join our crowd and represent your local communities interests and culture, and label or collect data to train artificial intelligence (AI) that works for everyone. Chances are your friends and family might be using the very AI you helped train."
+    },
+    {
+        id:4,
+        img:"",
+        title:"Work with the best",
+        text:"Our experience spans more than 20 years working with global companies across technology, finance, automotive, healthcare, government, retail, and other industries."
     }
+
 ]
-
 export default data;
-
-console.log(data)
