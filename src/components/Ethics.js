a0eefb72c66d27ac042679ada4961112e3fb4b96
Jobwafula <jobwafulabg@gmail.com>
2023-03-05 13:56:17 +0300

    mapped ethics data

--- src/components/Ethics.js
+++ src/components/Ethics.js
@@ -1,12 +1,52 @@
 import React from 'react'
-import data from './data.js'
+import ethics from './Ethicsdata'
 
 export default function Ethics() {
+     //  styles
 
-    console.log(data);
+     const styles = {
+        ethics:{
+            color:"red"
+        },
+        par:{
+            fontSize:"2rem"
+        },
+        item:{
+            display:"inline-block",
+            width:"30%",
+            height:"100px",
+            padding:"20px",
+            
+        },
+        title:{
+            color:"red",
+            textTransform:"capitalize"
+
+        }
+
+
+    }
+
+    
+    // mapping data array ethics
+    const arrayelement = ethics.map((item)=>(
+        <div style={styles.item} key={item.id}>
+            <h2 style={styles.title}>{item.title}</h2>
+            <p style={styles.par}>{item.ethicmsg}</p>
+        </div>
+    ));
+
+
+
+
+   
   return (
     <div>
-      
+      <h1>Crowd Code <span style={styles.ethics}>Of Ethics</span></h1>
+
+      <div>
+        {arrayelement}
+      </div>
     </div>
   )
 }
