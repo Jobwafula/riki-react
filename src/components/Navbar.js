258ac4a4d4f89fbce93a04a28e2073c095af76ee
Jobwafula <jobwafulabg@gmail.com>
2023-03-05 13:57:30 +0300

    changed horizontal position

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 export default function Navbar() {
+
+  const styles ={
+    container:{
+      backgroundColor:'black'
+
+    },
+   ulcontainer:{
+    float:"right"
+   }
+  }
   return (
-    <div style={{backgroundColor:'black'}}>
+    <div style={styles.container}>
          <ul id='ul-nav'>
                 <li><i style={{color:'white'}} class="fab fa-twitter"></i></li>
                 <li> <i style={{color:'white'}} class="fab fa-youtube"></i> </li>
