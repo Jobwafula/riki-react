dffd12209f46d11158f30bab64b9e34035fe651b
Jobwafula <jobwafulabg@gmail.com>
2023-03-05 13:49:42 +0300

    first commit

---
+++ src/components/Ethicsdata.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+      id:1,
+      img:"",
+      title:"Fair Pay",
+      ethicmsg:"Our goal is to pay our crowd above minimum wage in every market around the world where we operate."
+  },
+  {
+      id:2,
+      img:"",
+      title:"inclusion",
+      ethicmsg:"A diverse, inclusive culture is vital to our mission of helping build better AI. We offer opportunities for individuals of all abilities and backgrounds."
+      
+  },
+  {
+      id:3,
+      img:"",
+      title:"Crowd Voice",
+      ethicmsg:"Our crowd has a valued voice at Appen and their feedback helps us to continuously improve."
+  },
+  {
+    id:4,
+    img:"",
+    title:"privacy and confidentiality",
+    ethicmsg:"Any information collected about the crowd is requested solely for the purposes of the project. We take precautions to protect that information and do not release private data on individuals to third parties without consent."
+  },
+  {
+    id:5,
+    img:"",
+    title:"communication",
+    ethicmsg:"We believe in helpful, transparent, and responsive lines of communication with our crowd."
+  },
+  {
+    id:6,
+    img:"",
+    title:"Well-Being",
+    ethicmsg:"We promote wellness, community, and connections through online forums and best practices."
+  }
+]
+export default data;
